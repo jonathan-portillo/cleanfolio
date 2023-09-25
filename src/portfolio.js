476e71b618b34137add67cb64160f9044b694b10
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-09-25 03:40:24 -0400

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
   name: 'Jonathan Portillo',
   role: 'Full Stack Web Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'Afro Latino New Yorker Looking to Break into the software field',
   resume: 'https://example.com',
   social: {
     linkedin: 'https://www.linkedin.com/in/jonathanportillo93/',
