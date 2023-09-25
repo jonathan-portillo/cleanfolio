c68b24f477fca0883bdd55eab8b33762d811ebeb
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-09-25 02:24:15 -0400

    clean

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'localhost:3000',
+  title: 'JP.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Jonathan Portillo',
+  role: 'Full Stack Web Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/jonathanportillo93/',
+    github: 'https://github.com/jonathan-portillo',
   },
 }
 
@@ -21,12 +21,12 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Task Note',
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    stack: ['JavaScript', 'React', 'Node.js', 'Express'],
+    sourceCode: 'https://github.com/jonathan-portillo/todo-frontend-',
+    livePreview: 'https://task-notes-omega.vercel.app/',
   },
   {
     name: 'Project 2',
@@ -65,7 +65,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'jonport93@mail.com',
 }
 
 export { header, about, projects, skills, contact }
